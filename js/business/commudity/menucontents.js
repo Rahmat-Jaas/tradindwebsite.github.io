0f0d89411460a7089bd83845d69f388c39b67fe7
Rahmat-Jaas <mdrahmatullah9534@gmail.com>
2023-03-18 11:40:14 +0530

    Trading info website

---
+++ js/business/commudity/menucontents.js
@@ -0,0 +1,13 @@
+var anylinkmenu1 = { divclass: 'anylinkmenu', inlinestyle: '', linktarget: '_blank' }
+anylinkmenu1.items=[
+	["Online Application Form", "https://ibbs.bseindia.com/asbaforms/BidEntryQuick.aspx?ipo=964"],
+    ["Download Blank Application Form", "https://ibbs.bseindia.com/asbaforms/BlankForm.aspx?ipo=964"],
+    ["Download Revision Application Form", "https://ibbs.bseindia.com/asbaforms/SrchBid.aspx?ipo=964"]
+]
+var anylinkmenu2 = { divclass: 'anylinkmenu', inlinestyle: '', linktarget: '_blank' }
+anylinkmenu2.items = [
+    ["Disclosures under regulation 46 of LODR", "http://www.amrapali.co.in/investors.aspx"],
+    
+    
+]
+
